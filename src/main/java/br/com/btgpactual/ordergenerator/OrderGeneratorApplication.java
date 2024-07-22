5735aa3788c819818ea0835964fde3cd014e78b6
wscatao <wscatao@gmail.com>
2024-07-22 14:19:12 -0300

    Order Generator API

---
+++ src/main/java/br/com/btgpactual/ordergenerator/OrderGeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.btgpactual.ordergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderGeneratorApplication.class, args);
+	}
+
+}
